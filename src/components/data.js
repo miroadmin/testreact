9045e3b4c34c82a5cfa96bf74e1c008d0ce7eb49
miroadmin <46491133+miroadmin@users.noreply.github.com>
2022-08-22 20:11:08 +0200

    sum2

--- src/components/data.js
+++
@@ -1,3 +0,0 @@
-const Data =  [{FirstName: 'Joe', LastName: 'Caputo', ContactNumber: '07658312387', CampaignName: 'Black Rain', Sessions: 'Black Rain'},
-{FirstName: 'Joe', LastName: 'Caputo', ContactNumber: '07658312387', CampaignName: 'Black Rain', Sessions: 'Black Rain'}]
-export default Data;
--- src/components/playerslist.js
+++ src/components/playerslist.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import './playerslist.css';
 
 const customer =  [{FirstName: 'Joe', LastName: 'Caputo', ContactNumber: '07658312387', CampaignName: 'Black Rain', Sessions: 'Black Rain'},
 {FirstName: 'Piper', LastName: 'Chapman', ContactNumber: '07142548798', CampaignName: 'Black Rain', Sessions: 'One Last Riddle'},
 {FirstName: 'Tasha', LastName: 'Jefferson', ContactNumber: '07998987220', CampaignName: 'Black Rain', Sessions: 'The Burning Plague'},
 {FirstName: 'Gloria', LastName: 'Mendoza', ContactNumber: '07512645873', CampaignName: 'Black Rain', Sessions: 'The Sea Witch'},
-{FirstName: 'Theodore', LastName: 'Bagwell', ContactNumber: '07561384896', CampaignName: 'One Last Riddle', Sessions: 'Tomb of Horrors'},
-
-]
+{FirstName: 'Theodore', LastName: 'Bagwell', ContactNumber: '07561384896', CampaignName: 'One Last Riddle', Sessions: 'Tomb of Horrors'}]
 
 const ListPlayers = () => {
-    return
-    <>
-    </>
+    return (
+        <>
+            <div className='list'>
+                <span>First Name</span>
+                <span>Last Name</span>
+                <span>Contact Number</span>
+                <span>Campaign Name</span>
+                <span>Session</span>
+            </div>
+            <hr/>
+        </>
+    )
 }
 
 export default ListPlayers;
