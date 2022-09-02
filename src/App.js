b2738ad8cccf5574c11f5f510d4e531aa18eb65d
miroadmin <46491133+miroadmin@users.noreply.github.com>
2022-09-02 17:25:28 +0200

    Signed-off-by: miroadmin <46491133+miroadmin@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {Routes, Route, Link, BrowserRouter} from 'react-router-dom';
-import NewPlayer from "./components/newplayer"
-import EditPlayer from "./components/editplayer"
-import ListPlayers from "./components/playerslist"
+import NewPlayer from "./components/newplayer";
+import EditPlayer from "./components/editplayer";
+import ListPlayers from "./components/playerslist";
 import {MdViewList, MdPlaylistAdd } from "react-icons/md";
 import './App.css';
 
@@ -9,7 +9,26 @@ var customers =  [{FirstName: 'Joe', LastName: 'Caputo', ContactNumber: '0765831
 {FirstName: 'Piper', LastName: 'Chapman', ContactNumber: '07142548798', CampaignName: 'Black Rain', Sessions: 'One Last Riddle'},
 {FirstName: 'Tasha', LastName: 'Jefferson', ContactNumber: '07998987220', CampaignName: 'Black Rain', Sessions: 'The Burning Plague'},
 {FirstName: 'Gloria', LastName: 'Mendoza', ContactNumber: '07512645873', CampaignName: 'Black Rain', Sessions: 'The Sea Witch'},
-{FirstName: 'Theodore', LastName: 'Bagwell', ContactNumber: '07561384896', CampaignName: 'One Last Riddle', Sessions: 'Tomb of Horrors'}]
+{FirstName: 'Theodore', LastName: 'Bagwell', ContactNumber: '07561384896', CampaignName: 'One Last Riddle', Sessions: 'Tomb of Horrors'},
+{FirstName: 'Brad', LastName: 'Bellick', ContactNumber: '07883256418', CampaignName: 'One Last Riddle',Sessions: ''},
+{FirstName: 'Lincoln', LastName: 'Burrows', ContactNumber: '07112356983', CampaignName: 'One Last Riddle',Sessions: ''},
+{FirstName: 'Fernando', LastName: 'Sucre', ContactNumber: '07963212321', CampaignName: 'One Last Riddle',Sessions: ''},
+{FirstName: 'Sara', LastName: 'Tancredi', ContactNumber: '07954186684', CampaignName: 'One Last Riddle',Sessions: ''},
+{FirstName: 'Daryl', LastName: 'Dixon', ContactNumber: '07325649845', CampaignName: 'The Burning Plague',Sessions: ''},
+{FirstName: 'Maggie', LastName: 'Greene', ContactNumber: '07459832185', CampaignName: 'The Burning Plague',Sessions: ''},
+{FirstName: 'Carol', LastName: 'Peletier', ContactNumber: '07989444568', CampaignName: 'The Burning Plague',Sessions: ''},
+{FirstName: 'Eugene', LastName: 'Porter', ContactNumber: '07774854987', CampaignName: 'The Burning Plague',Sessions: ''},
+{FirstName: 'Billy', LastName: 'Cranston', ContactNumber: '007845222547', CampaignName: 'The Sea Witch',Sessions: ''},
+{FirstName: 'Kimberly', LastName: 'Hart', ContactNumber: '07815307459', CampaignName: 'The Sea Witch',Sessions: ''},
+{FirstName: 'Trini', LastName: 'Kwan', ContactNumber: '07548755285', CampaignName: 'The Sea Witch',Sessions: ''},
+{FirstName: 'Tommy', LastName: 'Oliver', ContactNumber: '07989444568', CampaignName: 'The Sea Witch',Sessions: ''},
+{FirstName: 'Jason', LastName: 'Scott', ContactNumber: '07774854987', CampaignName: 'The Sea Witch',Sessions: ''},
+{FirstName: 'Zack', LastName: 'Taylor', ContactNumber: '07845222547', CampaignName: 'The Sea Witch',Sessions: ''},
+{FirstName: 'Joyce', LastName: 'Byers', ContactNumber: '07954668187', CampaignName: 'Tomb of Horrors',Sessions: ''},
+{FirstName: 'Dustin', LastName: 'Henderson', ContactNumber: '07889554857', CampaignName: 'Tomb of Horrors',Sessions: ''},
+{FirstName: 'Jim', LastName: 'Hopper', ContactNumber: '07954845148', CampaignName: 'Tomb of Horrors',Sessions: ''},
+{FirstName: 'Nancy', LastName: 'Wheeler', ContactNumber: '07445845711', CampaignName: 'Tomb of Horrors',Sessions: ''},
+]
 
 function App() {
   return (
--- src/components/editplayer.js
+++ src/components/editplayer.js
@@ -1,19 +1,84 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import {Link, useLocation} from 'react-router-dom';
 
 import {FaRegEdit} from "react-icons/fa";
+import {RiArrowGoBackFill} from "react-icons/ri";
+import {HiSave} from "react-icons/hi";
+import './editplayer.css';
 
 
 const EditPlayer = () => {
     const location=useLocation();
-    console.log(location.state.customer)
-    return (
-    <>
-        <div><Link to="/" ><FaRegEdit /> Back </Link></div>
+    const [firstName, setFirstName] = useState(location.state.customer.FirstName);
+    const [lastName, setLastName] = useState(location.state.customer.LastName);
+    const [contactNumber, setContactNumber] = useState(location.state.customer.ContactNumber);
+    const [campaignName, setCampaignName] = useState(location.state.customer.CampaignName);
+    const [sessions, setSessions] = useState(location.state.customer.Sessions);
 
-      <div> {location.state.key} </div>
-      <div> {location.state.customer.FirstName} </div>
-    </>
+    const save = (event) => {
+      <Link to="/" ><HiSave /> Submit </Link>
+      // event.preventDefault();
+      // createUser();
+    }
+
+    return (
+      <>
+        <div className='container'>
+            <form onSubmit={save}>
+                <div className='form_element'>
+                    <label>First Name</label>
+                    <input 
+                        type='text' 
+                        value={firstName}
+                        onChange={ (e) => setFirstName(e.target.value)}/>
+                </div>
+
+                <div className='form_element'>
+                    <label>Last Name</label>
+                    <input 
+                        type='text' 
+                        value={lastName}
+                        onChange={ (e) => setLastName(e.target.value)}/>
+                </div>
+
+                <div className='form_element'>
+                    <label>Contact Number</label>
+                    <input 
+                        type='text' 
+                        value={contactNumber}
+                        onChange={ (e) => setContactNumber(e.target.value)}/>
+                </div>
+
+                <div className='form_element'>
+                    <label>Campaign Name</label>
+                    <select value={campaignName} 
+                      onChange={(e) => setCampaignName(e.target.value)}>
+                        <option value="Black Rain">Black Rain</option>
+                        <option value="One Last Riddle">Black Rain</option>
+                        <option value="The Burning Plague">The Burning Plague</option>
+                        <option value="The Sea Witch">The Sea Witch</option>
+                        <option value="Tomb of Horrors">Tomb of Horrors</option>
+                        <option value=""></option>
+                    </select>   
+                </div>
+
+                <div className='form_element'>
+                    <label>Sessions</label>
+                    <select value={sessions} 
+                      onChange={(e) => setSessions(e.target.value)}>
+                        <option value="Black Rain">Black Rain</option>
+                        <option value="One Last Riddle">One Last Riddle</option>
+                        <option value="The Burning Plague">The Burning Plague</option>
+                        <option value="The Sea Witch">The Sea Witch</option>
+                        <option value="Tomb of Horrors">Tomb of Horrors</option>
+                        <option value=""></option>
+                    </select>  
+                </div>
+                <button type='submit'>Submit</button>
+                {/* <button onClick={save}>Submit</button> */}
+            </form>
+        </div>    
+      </>
     )
 }
 
